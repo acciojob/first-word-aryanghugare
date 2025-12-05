1c2a49f3f931fc9df89aa4f2984b06110e0a3041
Aryan Ghugare <aryanghugare75@gmail.com>
2025-12-05 23:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function firstWord(s) {
   // your code here
-}
+  s =s.trim();
+  let ans = ""
+  for(let i = 0 ; i < s.length ; i++){
+      if(s[i]==" ") return ans ;
+      ans+=s[i];
+      
+  }
+  
+  return ans ; 
+}  
 
 // Do not change the code below
 
